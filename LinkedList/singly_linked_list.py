93b0e89685dc6828a8f3eb59c0a0079d71b70ec1
sumit singhal <sumitsinghal4390@gmail.com>
2017-12-16 12:48:30 +0530

    added baseLL and SLL

--- LinkedList/singly_linked_list.py
+++ LinkedList/singly_linked_list.py
@@ -1,14 +1,28 @@
 from LinkedList.base_classes import Node, BaseLinkedList
 
 class SinglyLinkedList(BaseLinkedList):
+
+    def add_node(self, element):
+
+        if self.is_linkedlist_empty():
+            self._set_first_node(element)
+            return
+        node = self.create_node(element)
+        last_node = self._get_last_node()
+        last_node.set_next(node)
+
     def add_nodes(self, elements):
         if elements and self.is_linkedlist_empty():
             self._set_first_node(elements.pop(0))
-
         last_node = self._get_last_node()
-
         for item in elements:
             last_node = self._set_and_get_last_node(item, last_node)
+
+    def print_linked_list(self, end_point=None):
+        if self.is_linkedlist_empty():
+            print('No nodes in list')
+            return
+        print(self._get_formatted_list(end_point))
 
     def _set_and_get_last_node(self, item, last_node):
         node = self.create_node(item)
@@ -19,16 +33,6 @@ class SinglyLinkedList(BaseLinkedList):
         node = self.create_node(item)
         self.head = node
 
-    def add_node(self, element):
-
-        if self.is_linkedlist_empty():
-            self._set_first_node(element)
-            return
-
-        node = self.create_node(element)
-        last_node = self._get_last_node()
-        last_node.set_next(node)
-
     def _get_formatted_list(self, end_point):
         current_node = self.head
         linklist = self.start_sign
@@ -37,12 +41,6 @@ class SinglyLinkedList(BaseLinkedList):
             current_node = current_node.get_next()
         linklist += self.end_sign
         return linklist
-
-    def print_linked_list(self, end_point=None):
-        if self.is_linkedlist_empty():
-            print('No nodes in list')
-            return
-        print(self._get_formatted_list(end_point))
 
     def _get_last_node(self):
         current_node = self.head
